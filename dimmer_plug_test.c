33c746f440b2f60583cb34800fe69a0c0116fce2
tychon <tychon4.2@googlemail.com>
2013-05-11 15:22:45 +0200

    added test for dimmer plug

--- dimmer_plug_test.c
+++ dimmer_plug_test.c
@@ -1,14 +1,21 @@
 
 // Test dimmer plug on PORT 2:
-// Blink LED0 with raising brightnesss
+// Turn LEDs 0 to 15 on and off in some different patterns.
 
 #include <util/delay.h>
 
 #include "jeenode_pins.h"
 #include "i2c.h"
 
+#include "plugs/dimmer_plug.c"
+
 #define ADDRESS 0x40
 
+void info() {
+  digital_on(DPORT, JP4D);
+  _delay_ms(10);
+  digital_off(DPORT, JP4D);
+}
 void fail() {
   digital_on(DPORT, JP4D);
   _delay_ms(1000);
@@ -17,23 +24,73 @@ void fail() {
 
 int main(void) {
   pin_as_output(DDDR, JP4D);
+  info();
   
   I2CPORT2(port);
   i2c_init(&port);
   
   I2C_HOLD;
-  i2c_register_write(&port, ADDRESS, 0x00, 0x00); // MODE1
-  i2c_register_write(&port, ADDRESS, 0x01, 0x20); // MODE2
-  i2c_register_write(&port, ADDRESS, 0x12, 0x40); // GRPPWM
-  i2c_register_write(&port, ADDRESS, 0x13,    5); // GRPFREQ
-  i2c_register_write(&port, ADDRESS, 0x14, 0x03); // LEDOUT0
+  // turn on oscillator
+  i2c_register_write(&port, ADDRESS, MODE1, 0x00);
+  // turn on all LED drivers
+  for (int i = 0; i < 4; i++)
+    i2c_register_write(&port, ADDRESS, LEDOUT(i), LDR_PWM_ALL);
   i2c_stop(&port);
   
-  char brightness = 0xA0;
-  while(true) {
-    i2c_register_write(&port, ADDRESS, 0x02, ++brightness);
+  unsigned char brightness[16];
+  for (int i = 0; i < 16; i++) brightness[i] = 0;
+  
+  // turn on
+  while(brightness[15] < 255) {
+    for (int i = 0; i < 16; i++) {
+      if ((i == 0) || (i > 0 && brightness[i-1] > 50)) {
+        if (brightness[i] < 255) {
+          brightness[i] ++;
+          if (! i2c_register_write(&port, ADDRESS, PWM(i), brightness[i])) fail();
+        }
+      }
+    }
     i2c_stop(&port);
-    _delay_ms(30);
+    _delay_ms(2);
   }
+  info();
+  
+  // turn off
+  for(int b = 255; b > 0; b --) {
+    for(int i = 0; i < 16; i++) {
+      i2c_register_write(&port, ADDRESS, PWM(i), b);
+      i2c_stop(&port);
+    }
+  }
+  info();
+  
+  // turn on
+  for (int i = 15; i >= 0; i--) {
+    for (int j = 0; j <= i; j ++) {
+      if (j > 0) i2c_register_write(&port, ADDRESS, PWM(j-1), 0x00);
+      i2c_register_write(&port, ADDRESS, PWM(j), 0xFF);
+      i2c_stop(&port);
+      _delay_ms(20);
+    }
+  }
+  info();
+  
+  // turn off
+  while (brightness[15] > 0) {
+    for (int i = 0; i < 16; i++) {
+      if ((i == 0) || (i > 0 && brightness[i-1] < 200)) {
+        if (brightness[i] > 0) {
+          brightness[i] --;
+          if (! i2c_register_write(&port, ADDRESS, PWM(i), brightness[i])) fail();
+        }
+      }
+    }
+    i2c_stop(&port);
+  }
+  info();
+  
+  //end
+  _delay_ms(10);
+  info();
 }
 
