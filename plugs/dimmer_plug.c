00a92793121978bffc0e7842d0273bb5581465b5
tychon <tychon4.2@googlemail.com>
2013-10-13 17:08:46 +0200

    add prefixes to dimmer plug macros

--- plugs/dimmer_plug.c
+++ plugs/dimmer_plug.c
@@ -1,37 +1,37 @@
 
 ///// Register addresses /////
-#define MODE1  0x00
-#define MODE2  0x01
-#define PWM(x) (0x02+(x))
-#define GRPPWM  0x12
-#define GRPFREQ 0x13
-#define LEDOUT(x) (0x14+(x))
-#define SUBADR(x) (0x18+(x))
-#define ALLCALLADR 0x1B
+#define DPMODE1  0x00
+#define DPMODE2  0x01
+#define DPPWM(x) (0x02+(x))
+#define DPGRPPWM  0x12
+#define DPGRPFREQ 0x13
+#define DPLEDOUT(x) (0x14+(x))
+#define DPSUBADR(x) (0x18+(x))
+#define DPALLCALLADR 0x1B
 
 ///// Auto-Increment options ////
 // to be ored with register addresses
-#define AUTOINCR_ALL (0x04 << 5)     // 0x00 to 0x1B
-#define AUTOINCR_PWM (0x05 << 5)     // 0x02 to 0x11
-#define AUTOINCR_GRP (0x06 << 5)     // 0x12 to 0x13
-#define AUTOINCR_PWM_GRP (0x07 << 5) // 0x02 to 0x13
+#define DPAUTOINCR_ALL (0x04 << 5)     // 0x00 to 0x1B
+#define DPAUTOINCR_PWM (0x05 << 5)     // 0x02 to 0x11
+#define DPAUTOINCR_GRP (0x06 << 5)     // 0x12 to 0x13
+#define DPAUTOINCR_PWM_GRP (0x07 << 5) // 0x02 to 0x13
 
 ///// LED driver options /////
 // Retrieve register address of driver for given LED index.
-#define LEDDRIVER(x) LEDOUT((x)/4)
+#define DPLEDDRIVER(x) LEDOUT((x)/4)
 // Retrieve position for LDR value in LEDOUT(x) register
-#define LDR_SHIFT(x) (((x)%4) * 2)
+#define DPLDR_SHIFT(x) (((x)%4) * 2)
 
-#define LDR_OFF(x)     (0x00 << LDR_SHIFT(x)) // LED off
-#define LDR_ON(x)      (0x01 << LDR_SHIFT(x)) // LED on
-#define LDR_PWM(x)     (0x02 << LDR_SHIFT(x)) // LED controlled by PWM(x)
-#define LDR_PWM_GRP(x) (0x03 << LDR_SHIFT(x)) // LED controlled by PWM(x) and GRP
+#define DPLDR_OFF(x)     (0x00 << LDR_SHIFT(x)) // LED off
+#define DPLDR_ON(x)      (0x01 << LDR_SHIFT(x)) // LED on
+#define DPLDR_PWM(x)     (0x02 << LDR_SHIFT(x)) // LED controlled by PWM(x)
+#define DPLDR_PWM_GRP(x) (0x03 << LDR_SHIFT(x)) // LED controlled by PWM(x) and GRP
 
 // set all drivers in one driver register to same value
-#define LDR_OFF_ALL     0x00
-#define LDR_ON_ALL      0x55 // = 0b01010101
-#define LDR_PWM_ALL     0xAA // = 0b10101010
-#define LDR_PWM_GRP_ALL 0xFF // = 0b11111111
+#define DPLDR_OFF_ALL     0x00
+#define DPLDR_ON_ALL      0x55 // = 0b01010101
+#define DPLDR_PWM_ALL     0xAA // = 0b10101010
+#define DPLDR_PWM_GRP_ALL 0xFF // = 0b11111111
 
 // Example for setting driver of LED12 and LED13 to PWM:
 // register address: LEDDRIVER(12)
--- plugs/dimmer_plug_test.c
+++ plugs/dimmer_plug_test.c
@@ -33,12 +33,12 @@ int main(void) {
   
   I2C_HOLD;
   // turn on oscillator
-  i2c_register_write(&port, ADDRESS, MODE1, 0x00);
+  i2c_register_write(&port, ADDRESS, DPMODE1, 0x00);
   // outputs change on ACK and not on STOP
-  i2c_register_write(&port, ADDRESS, MODE2, 0x0D);
+  i2c_register_write(&port, ADDRESS, DPMODE2, 0x0D);
   // turn on all LED drivers
   for (int i = 0; i < 4; i++)
-    i2c_register_write(&port, ADDRESS, LEDOUT(i), LDR_PWM_ALL);
+    i2c_register_write(&port, ADDRESS, DPLEDOUT(i), DPLDR_PWM_ALL);
   i2c_stop(&port);
   
   unsigned char brightness[16];
@@ -50,7 +50,7 @@ int main(void) {
       if ((i == 0) || (i > 0 && brightness[i-1] > 50)) {
         if (brightness[i] < 255) {
           brightness[i] ++;
-          i2c_register_write(&port, ADDRESS, PWM(i), brightness[i]);
+          i2c_register_write(&port, ADDRESS, DPPWM(i), brightness[i]);
         }
       }
     }
@@ -60,7 +60,7 @@ int main(void) {
   info();
   
   // turn off (using auto increment feature)
-  i2c_register_addr(&port, ADDRESS, AUTOINCR_PWM | PWM(0));
+  i2c_register_addr(&port, ADDRESS, DPAUTOINCR_PWM | DPPWM(0));
   for(int b = 255; b > 0; b --) {
     for(int i = 0; i < 16; i++)
       i2c_write(&port, b);
@@ -72,8 +72,8 @@ int main(void) {
   // turn on
   for (int i = 15; i >= 0; i--) {
     for (int j = 0; j <= i; j ++) {
-      if (j > 0) i2c_register_write(&port, ADDRESS, PWM(j-1), 0x00);
-      i2c_register_write(&port, ADDRESS, PWM(j), 0xFF);
+      if (j > 0) i2c_register_write(&port, ADDRESS, DPPWM(j-1), 0x00);
+      i2c_register_write(&port, ADDRESS, DPPWM(j), 0xFF);
       i2c_stop(&port);
       _delay_ms(22);
     }
@@ -82,14 +82,14 @@ int main(void) {
   
   // pseudo-randomness
   for (int i = 0; i < 255; i++) {
-    i2c_register_write(&port, ADDRESS, PWM((uint8_t)(xor128() % (uint32_t)16)), (uint8_t)(xor128() % (uint32_t)16));
+    i2c_register_write(&port, ADDRESS, DPPWM((uint8_t)(xor128() % (uint32_t)16)), (uint8_t)(xor128() % (uint32_t)16));
     _delay_ms(10);
   }
   i2c_stop(&port);
   info();
   
   // all off
-  i2c_register_addr(&port, ADDRESS, AUTOINCR_PWM | PWM(0));
+  i2c_register_addr(&port, ADDRESS, DPAUTOINCR_PWM | DPPWM(0));
   for (int i = 0; i < 16; i++) i2c_write(&port, 0x00);
   i2c_stop(&port);
   info();
@@ -97,7 +97,7 @@ int main(void) {
   // (twinkling)
   char twinkling[16];
   for (int i = 0; i < 16; i++) twinkling[i] = 0, brightness[i] = 0;
-  i2c_register_addr(&port, ADDRESS, AUTOINCR_PWM | PWM(0));
+  i2c_register_addr(&port, ADDRESS, DPAUTOINCR_PWM | DPPWM(0));
   for (uint16_t i = 0; i < 10000; i++) {
     if (i % 100 == 0) { // choose one random twinkling star
       int pos = (uint8_t)(xor128() % (uint32_t)16);
@@ -114,7 +114,7 @@ int main(void) {
   info();
   
   // all on
-  i2c_register_addr(&port, ADDRESS, AUTOINCR_PWM | PWM(0));
+  i2c_register_addr(&port, ADDRESS, DPAUTOINCR_PWM | DPPWM(0));
   for (int i = 0; i < 16; i++) i2c_write(&port, 0xFF), brightness[i] = 255;
   i2c_stop(&port);
   _delay_ms(500);
@@ -126,7 +126,7 @@ int main(void) {
       if ((i == 0) || (i > 0 && brightness[i-1] < 200)) {
         if (brightness[i] > 0) {
           brightness[i] --;
-          if (! i2c_register_write(&port, ADDRESS, PWM(i), brightness[i])) fail();
+          if (! i2c_register_write(&port, ADDRESS, DPPWM(i), brightness[i])) fail();
         }
       }
     }
