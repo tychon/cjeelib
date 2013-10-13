3a1993b7e07b602e6a636dfed05c5934a6b02235
tychon <tychon4.2@googlemail.com>
2013-10-13 16:35:45 +0200

    make baud rate configurable in reader.c

--- host/reader.c
+++ host/reader.c
@@ -5,13 +5,76 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+void print_help(char *path) {
+  fprintf(stderr, "usage: %s [-b BAUDRATE] DEVICE\n", path);
+  fputs("supported baud rates:\n"
+      "  B50\n"
+      "  B75\n"
+      "  B110\n"
+      "  B134\n"
+      "  B150\n"
+      "  B200\n"
+      "  B300\n"
+      "  B600\n"
+      "  B1200\n"
+      "  B1800\n"
+      "  B2400\n"
+      "  B4800\n"
+      "  B9600\n"
+      "  B19200\n"
+      "  B38400\n"
+      "  B57600\n"
+      "  B115200\n"
+      "  B230400\n"
+      "  B500000\n"
+      "  B576000\n"
+      "  B1000000\n\n", stderr);
+  exit(1);
+}
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s DEVICE\n", argv[0]);
-    exit(1);
+  setbuf(stdout, NULL);
+  
+  char *device = NULL;
+  int baudrate = B57600;
+  for (int i = 1; i < argc; i++) {
+    if (! strcmp(argv[i], "-b")) {
+      if (++i >= argc) print_help(argv[0]);
+      #define TRY(rate) if (!strcmp(argv[i], #rate)) baudrate = rate;
+           TRY(B50)
+      else TRY(B75)
+      else TRY(B110)
+      else TRY(B134)
+      else TRY(B150)
+      else TRY(B200)
+      else TRY(B300)
+      else TRY(B600)
+      else TRY(B1200)
+      else TRY(B1800)
+      else TRY(B2400)
+      else TRY(B4800)
+      else TRY(B9600)
+      else TRY(B19200)
+      else TRY(B38400)
+      else TRY(B57600)
+      else TRY(B115200)
+      else TRY(B230400)
+      else TRY(B500000)
+      else TRY(B576000)
+      else TRY(B1000000)
+      else print_help(argv[0]);
+    } else if (! device) {
+      device = argv[i];
+    } else print_help(argv[0]);
   }
-  char *device = argv[1];
+  if (! device) {
+    fputs("Error: missing path to device\n\n", stderr);
+    print_help(argv[0]);
+  }
+  fprintf(stderr, "baud rate termios bits: 0x%x\n", baudrate);
+  
   
   int serial_fd = open(device, O_RDONLY | O_NOCTTY);
   struct termios config;
@@ -22,12 +85,10 @@ int main(int argc, char *argv[]) {
   config.c_cflag |= CS8;
   config.c_cc[VMIN]  = 1;
   config.c_cc[VTIME] = 0;
-  //TODO make baud rate configurable by command line
-  if(cfsetispeed(&config, B57600) < 0 || cfsetospeed(&config, B57600) < 0) exit(1);
+  if(cfsetispeed(&config, baudrate) < 0 || cfsetospeed(&config, baudrate) < 0) exit(1);
   if(tcsetattr(serial_fd, TCSANOW, &config) < 0) exit(1);
   FILE *serial = fdopen(serial_fd, "r");
   
-  setbuf(stdout, NULL);
   setbuf(serial, NULL);
   
   char l[255];
