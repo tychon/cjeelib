19892505b4b4bd6de099da9abc56191170733973
tychon <tychon4.2@googlemail.com>
2013-10-16 20:37:23 +0200

    offer defines for bit selectors like THREEBITS 0x07

--- bits.h
+++ bits.h
@@ -12,5 +12,12 @@
 #define BITSET(reg, bit) (bit_is_set(reg, bit))
 #define BITCLR(reg, bit) (bit_is_clear(reg, bit))
 
+#define TWOBITS   0x03 //      0b11
+#define THREEBITS 0x07 //     0b111
+#define FOURBITS  0x0f //    0b1111
+#define FIVEBITS  0x1f //   0b11111
+#define SIXBITS   0x3f //  0b111111
+#define SEVENBITS 0x7f // 0b1111111
+
 #endif // _BITBEATER_H
 
